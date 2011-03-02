5af9d0ead2122b602890a8606cf0dbfd3477f54b
bombela <bombela@gmail.com>
2011-03-02 06:40:15 +0100

    flexible configuration

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,14 @@ for service in $SERVICES
 do
 	case $service in
 		worker)
+			cp -f redisconfig.py celeryconfig.py worker/
 			for i in $(seq 3)
 			do
 				dotcloud push spell.worker$i worker &
 			done
 			;;
 		train)
+			cp -f redisconfig.py celeryconfig.py train/
 			dotcloud push spell.train train &
 			;;
 		*)
